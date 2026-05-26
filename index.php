a7a258dc18f12b2eea104d32c106f2784324db47
Bigxbling <bigxbling@gmail.com>
2026-05-26 12:41:26 +0000

    Ajout du frontend de base

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gestion Hôtel</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <h1>Gestion d'Hôtel</h1>
+
+    <a href="pages/login.php">
+        <button>Connexion</button>
+    </a>
+
+</body>
+</html>
